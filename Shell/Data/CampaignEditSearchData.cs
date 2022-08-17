6763ff5f0d574dcd67801fb9a16a35a62a1689e2
devatb <dev.atb@outlook.com>
2022-08-17 17:43:36 +0700

    qd

---
+++ Shell/Data/CampaignEditSearchData.cs
@@ -0,0 +1,12 @@
+﻿namespace Shell.Data
+{
+    public class CampaignEditSearchData
+    {
+        
+        public string? SalesTextCode { get; set; }
+        public string? MaterialCode { get; set; }
+        public string? ProductName { get; set; }
+        public string? Point { get; set; }
+        public string? ACodeUse { get; set; }
+    }
+}
