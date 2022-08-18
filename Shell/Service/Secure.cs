c9c614f2d60d75ea4e108f125303a1ebdb722ed3
devatb <dev.atb@outlook.com>
2022-08-18 18:26:52 +0700

    sad

--- Shell/Service/Secure.cs
+++ Shell/Service/Secure.cs
@@ -7,7 +7,7 @@ namespace Shell.Service
     public interface ISecure
     {
         void restart();
-        void Page_Init();
+        Task<bool> Page_Init();
         Task<int> Check_Menu_Authorize(string cma_id);
         Task<int> Check_Document_Authorize(string cda_id, string per_id);
     }
@@ -25,14 +25,24 @@ namespace Shell.Service
             _sessionStorage.ClearAsync();
             _navMagager.NavigateTo("/login");
         }
-        public async void Page_Init()
+        public async Task<bool> Page_Init()
         {
-            var session_user = await _sessionStorage.GetItemAsync<string>("UserID");
-            if (string.IsNullOrWhiteSpace(session_user))
+            try
             {
-                //Response.Redirect("invalid");
-                _navMagager.NavigateTo("/invalid");
+                var session_user = await _sessionStorage.GetItemAsync<string>("UserID");
+                if (string.IsNullOrWhiteSpace(session_user))
+                {
+                    //Response.Redirect("invalid");
+                    _navMagager.NavigateTo("/invalid");
+                    return false;
+                }
             }
+            catch (Exception)
+            {
+                _navMagager.NavigateTo("/invalid");
+                return false;
+            }
+            return true;
         }
 
         public async Task<int> Check_Menu_Authorize(string cma_id)
